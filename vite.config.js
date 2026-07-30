eb326bd2d0d232d6481809ce9862f5f6b599e71f
AlvisWu <alvis.wu@iisigroup.com>
2026-07-30 08:59:56 +0800

    [COMMON][Alvis][修正開發指南 Pages 資源路徑]

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
-  base: '/frontend-development-guide/',
+  base: '/RdssDevelopeGuide/',
   plugins: [vue()],
   resolve: {
     alias: {
